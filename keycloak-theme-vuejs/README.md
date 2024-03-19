8798a61186139fefc128da8b4979e0950502e98b
Mélanie Rodrigues <melanie.rodrigues@comparaja.pt>
2024-03-19 17:37:13 +0000

    workin on new theme

---
+++ keycloak-theme-vuejs/README.md
@@ -0,0 +1,5 @@
+# Keycloak Theme with Vue 3 and Typescript
+
+- You need to have docker installed on your development environment
+- Follow the instructions bellow to run this theme
+- https://youtu.be/qrjjiG1206E ( tutorial pt-br )
---
+++ keycloak-theme-vuejs/src/@types/environment.d.ts
@@ -0,0 +1,106 @@
+interface EnvUrl {
+  registration: string
+  loginResetCredentials: string
+  login: string
+  loginAction: string
+  registrationAction: string
+}
+
+interface EnvTitle {
+  loginProfileTitle: string
+  loginAccountTitle: string
+  registerTitle: string
+  emailForgotTitle: string
+  confirmLinkIdpTitle: string
+  emailLinkIdpTitle: string
+}
+
+interface EnvPermission {
+  usernameEditDisabled: boolean
+  loginWithEmailAllowed: boolean
+  registrationEmailAsUsername: boolean
+  rememberMe: boolean
+  resetPasswordAllowed: boolean
+  password: boolean
+  registrationAllowed: boolean
+  registrationDisabled: boolean
+  passwordRequired: boolean
+}
+
+interface EnvLabel {
+  firstName: string
+  lastName: string
+  username: string
+  usernameOrEmail: string
+  email: string
+  password: string
+  passwordConfirm: string
+  rememberMe: string
+  doForgotPassword: string
+  doLogIn: string
+  doSubmit: string
+  noAccount: string
+  doRegister: string
+  backToLogin: string
+  confirmLinkIdpContinue: string
+  doClickHere: string
+}
+
+interface EnvForm {
+  loginUsername: string
+  loginRememberMe: boolean
+  selectedCredential: string
+  registerFirstName: string
+  registerLastName: string
+  registerEmail: string
+  registerUsername: string
+}
+
+interface EnvUser {
+  username: string
+  email: string
+  firstName: string
+  lastName: string
+}
+
+interface EnvValidation {
+  firstName: string
+  lastName: string
+  email: string
+  usernameOrPassword: string
+  username: string
+  password: string
+  passwordConfirm: string
+}
+
+interface EnvMessage {
+  type: string
+  sumary: string
+}
+
+interface EnvInstruction {
+  emailLinkIdp1: string
+  emailLinkIdp2: string
+  emailLinkIdp3: string
+  emailLinkIdp4: string
+  emailLinkIdp5: string
+}
+
+interface EnvSocial {
+  alias: string
+  displayName: string
+  loginUrl: string
+}
+
+interface Environment {
+  urls: EnvUrl
+  titles: EnvTitle
+  permissions: EnvPermission
+  labels: EnvLabel
+  forms: EnvForm
+  user: EnvUser
+  validations: EnvValidation
+  message: EnvMessage
+  social: EnvSocial[]
+  instruction: EnvInstruction
+}
